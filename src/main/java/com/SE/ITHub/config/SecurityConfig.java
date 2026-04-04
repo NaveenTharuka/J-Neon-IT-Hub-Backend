99bd70b71bdf7a9b2af8a118169c7a0ecb709879
NaveenTharuka <jnaveen829@gmail.com>
2026-04-04 20:02:58 +0530

    Fix config files

--- src/main/java/com/SE/ITHub/config/SecurityConfig.java
+++ src/main/java/com/SE/ITHub/config/SecurityConfig.java
@@ -17,8 +17,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private String frontendUrl = "http://localhost:5173";
-    
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Autowired
     private CustomOAuth2UserService customOAuth2UserService; // ADD THIS
 
