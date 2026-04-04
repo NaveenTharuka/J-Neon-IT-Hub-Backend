f4dd95f878911356d9a7a322e52fbab0c88db88d
NaveenTharuka <jnaveen829@gmail.com>
2026-04-04 19:51:55 +0530

    Fix test files

--- src/test/java/com/SE/ITHub/ItHubForAClientApplicationTests.java
+++ src/test/java/com/SE/ITHub/ItHubForAClientApplicationTests.java
@@ -2,16 +2,15 @@ package com.SE.ITHub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-<<<<<<< HEAD
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
-=======
-
-@SpringBootTest
->>>>>>> 793ed9b (feat(contact): getting contact by ID)
+@TestPropertySource(properties = {
+		"spring.sql.init.mode=never",
+		"flyway.enabled=false"
+})
 class ItHubForAClientApplicationTests {
 
 	@Test
